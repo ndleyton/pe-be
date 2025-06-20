7bcbca4058aee3d0f724bfba3f63d33ee59313ed
NICOLAS LEYTON <ndleyton@uc.cl>
2025-06-20 13:00:54 -0400

    refactor: Update Vite configuration to use file URL imports
    - Replaced `path` module with `fileURLToPath` and `URL` from `node:url` for resolving path aliases.
    - Improved compatibility with ES modules in the Vite configuration, enhancing project structure and maintainability.

--- pe-be-tracker-frontend/vite.config.js
+++ pe-be-tracker-frontend/vite.config.js
@@ -2,19 +2,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@/shared': path.resolve(__dirname, './src/shared'),
-      '@/features': path.resolve(__dirname, './src/features'),
-      '@/app': path.resolve(__dirname, './src/app'),
-      '@/utils': path.resolve(__dirname, './src/utils'),
-      '@/contexts': path.resolve(__dirname, './src/contexts'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@/shared': fileURLToPath(new URL('./src/shared', import.meta.url)),
+      '@/features': fileURLToPath(new URL('./src/features', import.meta.url)),
+      '@/app': fileURLToPath(new URL('./src/app', import.meta.url)),
+      '@/utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+      '@/contexts': fileURLToPath(new URL('./src/contexts', import.meta.url)),
     }
   },
   test: {
