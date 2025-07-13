306df90d7160efd465b206b75bd2fb9fb5605f5b
NICOLAS LEYTON <ndleyton@uc.cl>
2025-07-13 00:18:00 -0400

    made intensity unit nullable to get default value null
    default value null means the user gives it meaning to what the insensity is

---
+++ backend/alembic/versions/c2d4e6f7a8b9_make_default_intensity_unit_nullable.py
@@ -0,0 +1,28 @@
+"""make default_intensity_unit nullable
+
+Revision ID: c2d4e6f7a8b9
+Revises: b1c34d5e6f7b
+Create Date: 2025-07-13 00:10:00.000000
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'c2d4e6f7a8b9'
+down_revision = 'b1c34d5e6f7b'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    # Alter column to be nullable and drop default
+    op.alter_column('exercise_types', 'default_intensity_unit',
+                    existing_type=sa.Integer(),
+                    nullable=True, server_default=None)
+
+
+def downgrade():
+    # Revert column to NOT NULL with default 1 (matches original state)
+    op.alter_column('exercise_types', 'default_intensity_unit',
+                    existing_type=sa.Integer(),
+                    nullable=False, server_default='1') 
