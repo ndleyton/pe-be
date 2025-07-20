029e374d99f142ca909b22cf77ba06bd758ef374
NICOLAS LEYTON <ndleyton@uc.cl>
2025-07-20 17:05:27 -0400

    removed unnecessary or

--- pe-be-tracker-frontend/src/App.tsx
+++ pe-be-tracker-frontend/src/App.tsx
@@ -28,7 +28,7 @@ const App: React.FC = () => {
                 <div className="w-full border-t border-gray-300"></div>
               </div>
               <div className="relative flex justify-center text-sm">
-                <span className="px-2 bg-base-100 text-gray-500">or</span>
+                <span className="px-2 bg-base-100 text-gray-500"></span>
               </div>
             </div>
             
