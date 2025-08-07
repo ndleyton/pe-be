d311d521a49afe8095830e220ea8a03f0b0abba7
NICOLAS LEYTON <ndleyton@uc.cl>
2025-08-06 20:02:01 -0400

    change test env loading

--- pe-be-tracker-frontend/playwright.config.ts
+++ pe-be-tracker-frontend/playwright.config.ts
@@ -11,6 +11,12 @@ export default defineConfig({
     baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
   },
+  webServer: {
+    command: 'npm run dev:test',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
   projects: [
     {
       name: 'chromium',
