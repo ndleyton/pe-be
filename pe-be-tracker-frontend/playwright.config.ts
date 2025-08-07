40d14f6009459ff048e0cc597dde5cfc73fd0444
NICOLAS LEYTON <ndleyton@uc.cl>
2025-08-06 20:16:09 -0400

    removed webserver form playwright

--- pe-be-tracker-frontend/playwright.config.ts
+++ pe-be-tracker-frontend/playwright.config.ts
@@ -11,12 +11,7 @@ export default defineConfig({
     baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
   },
-  webServer: {
-    command: 'npm run dev:test',
-    url: 'http://localhost:5173',
-    reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
-  },
+  
   projects: [
     {
       name: 'chromium',
