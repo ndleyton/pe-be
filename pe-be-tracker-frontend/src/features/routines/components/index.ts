997ac8963ace53ae1ed96d47c28dcfef9958df45
NICOLAS LEYTON <ndleyton@uc.cl>
2025-08-09 12:35:03 -0400

    change routines to use exxisting card

--- pe-be-tracker-frontend/src/features/routines/components/index.ts
+++ pe-be-tracker-frontend/src/features/routines/components/index.ts
@@ -1,4 +1,3 @@
-export { RoutineCard } from './RoutineCard';
 export { RoutinesSection } from './RoutinesSection/RoutinesSection';
 export { SaveRoutineModal } from './SaveRoutineModal/SaveRoutineModal';
 export { RoutineQuickStartCard } from './RoutineQuickStartCard/RoutineQuickStartCard';
