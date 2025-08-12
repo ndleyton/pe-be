24674e1369c32c3127282fb0efbf6b829e39850a
NICOLAS LEYTON <ndleyton@uc.cl>
2025-08-12 01:00:02 -0400

    Refactor ExerciseList and WorkoutForm components for improved styling and layout consistency; adjust padding and margins for better responsiveness in mobile.

--- pe-be-tracker-frontend/src/features/workouts/pages/MyWorkoutsPage.tsx
+++ pe-be-tracker-frontend/src/features/workouts/pages/MyWorkoutsPage.tsx
@@ -146,7 +146,7 @@ const MyWorkoutsPage = () => {
 
   return (
     <>
-      <div className="max-w-5xl mx-auto p-8 text-center">
+      <div className="max-w-5xl mx-auto p-4 sm:p-8 text-center">
         <div className="max-w-4xl mx-auto">
           <div className="mb-6">
             <h1 className="text-2xl font-bold">Workouts</h1>
