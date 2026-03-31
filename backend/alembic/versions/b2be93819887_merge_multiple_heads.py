c4991058c2e697cc8c8589dd4b4adedfb37e09d4
ndleyton <ndleyton@uc.cl>
2026-03-31 15:19:03 -0300

    chore: fix duplicate head (#236)
    * feat: VPS image hosting and generation
    
    * feat: add a volume in containers for image hosting in VPS
    
    * chore: merge alembic heads
    
    * fix(frontend): remove duplicated admin endpoints block from auto-merge

---
+++ backend/alembic/versions/b2be93819887_merge_multiple_heads.py
@@ -0,0 +1,28 @@
+"""Merge multiple heads
+
+Revision ID: b2be93819887
+Revises: 1f4d2c8b9a10, 3c4d5e6f7a8b
+Create Date: 2026-03-31 14:58:54.728050
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'b2be93819887'
+down_revision: Union[str, None] = ('1f4d2c8b9a10', '3c4d5e6f7a8b')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
